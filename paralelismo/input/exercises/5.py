42f20c3f91a82515b0a947e1b88caf5b41e19fcd
marcosdayanm <marcosdayanmann@gmail.com>
2024-10-31 19:50:39 -0600

    deleted input files to read

--- paralelismo/input/exercises/5.py
+++
@@ -1,9 +0,0 @@
-# Pide al usuario dos números
-numero1 = float(input("Introduce el primer número: "))
-numero2 = float(input("Introduce el segundo número: "))
-
-# Calcula la suma de los números
-suma = numero1 + numero2
-
-# Muestra el resultado
-print("La suma de", numero1, "y", numero2, "es", suma)
--- paralelismo/input/exercises/6.py
+++
@@ -1,7 +0,0 @@
-# Pide al usuario un número
-numero = int(input("Introduce un número para ver su tabla de multiplicar: "))
-
-# Muestra la tabla de multiplicar del número del 1 al 10
-for i in range(1, 11):
-    resultado = numero * i
-    print(numero, "x", i, "=", resultado)
